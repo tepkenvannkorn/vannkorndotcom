dd7cb7d9696757018c16a726b9653d5e1f44a58e
vannkorn <vannkorn@gmail.com>
2021-07-02 20:55:34 +0700

    Add missing get_directory_uri() in header

--- header.php
+++ header.php
@@ -21,8 +21,8 @@
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-			<script src="js/html5shiv.min.js"></script>
-			<script src="js/respond.min.js"></script>
+			<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.min.js"></script>
+			<script src="<?php echo get_template_directory_uri(); ?>/js/respond.min.js"></script>
 		<![endif]-->
 
 	<?php wp_head(); ?>
